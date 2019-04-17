e2dc8da8950fc0b60817c79ef805eeb824757700
Mohamed Ali JAMAOUI <mohamedali.jamaoui@lfr10104.intra.rakuten.co.jp>
2019-04-17 17:19:17 +0200

    initial lopq integration

---
+++ ann_benchmarks/algorithms/lopq.py
@@ -0,0 +1,22 @@
+"""
+Implements the necessary adapters to integrate LOPQ in the set
+of benchmarked algorithms.
+"""
+import lopq
+from ann_benchmarks.algorithms.base import BaseANN
+
+
+class Lopq(BaseANN):
+	"""Implements the methods required to integrate LOPQ
+	to ann-benchmark framework"""
+	def query(self):
+		raise NotImplementedError
+
+	def batch_query(self, X, n):
+		raise NotImplementedError
+
+	def get_batch_results(self):
+		raise NotImplementedError
+
+	def fit(self, X):
+		raise NotImplementedError
